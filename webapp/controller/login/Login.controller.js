7efbe051e706c1f0cce376b9b2f283256644a99e
leandro-miguel <miguel.apmbb@gmail.com>
2021-08-11 23:06:04 -0300

    inserindo a tela de login

--- webapp/controller/login/Login.controller.js
+++ webapp/controller/login/Login.controller.js
@@ -24,55 +24,5 @@ sap.ui.define([
             oModel.setProperty("/ViewTitle", oBundle.getText("createClient"));
             this.setDateValues();
         },
-
-        _modelData: {
-			selectEnabled: true,
-			colorSet: "ColorSet5",
-			shades: [
-				CellColorShade.ShadeA,
-				CellColorShade.ShadeB,
-				CellColorShade.ShadeC,
-				CellColorShade.ShadeD,
-				CellColorShade.ShadeE,
-				CellColorShade.ShadeF
-
-			],
-			contrastCells: []
-		},
-
-		_fillModel: function (oData) {
-			var oModel = this.getView().getModel();
-			oModel.setData(oData);
-		},
-
-		handleChecked: function (oEvent) {
-			var bChecked = oEvent.getParameter("selected");
-
-			if (bChecked) {
-				this._fillModel(this._modelData);
-			} else {
-				this._modelData = this.getView().getModel().getData();
-				this._fillModel({ selectEnabled: false });
-			}
-		},
-
-		handleContrastCellSelection: function (oEvent) {
-			var oView = this.getView(),
-				oItem = oEvent.getParameter("changedItem"),
-				bSelected = oEvent.getParameter("selected"),
-				oBLCell = oView.byId(oItem.getKey());
-
-			if (!oBLCell) {
-				return;
-			}
-
-			if (bSelected) {
-				oBLCell.addStyleClass("sapContrast").addStyleClass("sapContrastPlus");
-			} else {
-				oBLCell.removeStyleClass("sapContrast").removeStyleClass("sapContrastPlus");
-			}
-		}
-
-
 	});
 });
